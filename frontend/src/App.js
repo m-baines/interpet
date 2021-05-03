7a26dcd55c36e99884af0ebac0f23707d1974785
JosephSimmons <josephsimmons@outlook.com>
2021-05-03 16:33:43 +0100

    favicon working, react-router-dom installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+// Import components
+
+// Import react stuff
+import { useState, useEffect } from 'react' 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 function App() {
+  // use states
+  // use effects
+  // functions e.g. onClick, onSearch
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <Router>
+      <div className="App">
+        <p>Hello</p>
+      </div>
+    </Router>
+    
   );
-}
+} 
 
 export default App;
